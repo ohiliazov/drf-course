19c98aa317341c4f9d49cb2af1781d64736cf5a8
Oleksandr Hiliazov <oleksandr.hiliazov@mac-VJK9RPYYDY.local>
2024-10-04 11:33:26 +0200

    update admin page

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -24,3 +24,13 @@ class AdminSiteTests(TestCase):
         res = self.client.get(url)
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        url = reverse("admin:core_user_change", args=[self.user.id])
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        url = reverse("admin:core_user_add")
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
